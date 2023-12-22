d13658d8506b448eee262641189a8e3b03652b33
Jhonata Gonçalves <jhsocial@icloud.com>
2023-12-22 13:10:21 +0000

    rank4 done!

--- microshell.c
+++ microshell.c
@@ -1,61 +1,69 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jhogonca <jhogonca@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/22 12:27:12 by jhogonca          #+#    #+#             */
+/*   Updated: 2023/12/22 12:59:07 by jhogonca         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <sys/wait.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 
-void err(char *str) 
-{
-    while (*str)
-        write(2, str++, 1);
+void error(char *str) {
+	write(2, str, strlen(str));
 }
 
-void cd(char **argv, int i) 
+void cd(char **av, int i) 
 {
-    if (i != 2)
-        return err("error: cd: bad arguments\n");
-    if (chdir(argv[1]) == -1)
-        return err("error: cd: cannot change directory to "), err(argv[1]), err("\n");
+	if (i != 2)
+		return error("error: cd: bad arguments\n");
+	if (chdir(av[1]) == -1)
+		return error("error: cd: cannot change directory to "), error(av[1]), error("\n");
 }
 
-void exec(char **argv, char **envp, int i) 
+void exec(char **av, char **envp, int i) 
 {
-    int fd[2];
-    int has_pipe = argv[i] && !strcmp(argv[i], "|");
+	int fd[2];
+	int has_pipe = av[i] && !strcmp(av[i], "|");
 
-    if (has_pipe && pipe(fd) == -1)
-        return err("error: fatal\n");
-    
-    int pid = fork();
-    if (!pid) 
-    {
-        argv[i] = 0;
-        if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-            return err("error: fatal\n");
-        execve(*argv, argv, envp);
-        return err("error: cannot execute "), err(*argv), err("\n");
-    }
-    waitpid(-1, 0, 0);
-    if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-        return err("error: fatal\n");
+	if (has_pipe && pipe(fd) == -1)
+		return error("error: fatal\n");
+
+	if (fork() == 0)
+	{
+		av[i] = 0;
+		if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+			return error("error: fatal\n");
+		execve(*av, av, envp);
+		return error("error: cannot execute "), error(*av), error("\n");
+	}
+	waitpid(-1, 0, 0);
+	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+		return error("error: fatal\n");
 }
 
-int main(int argc, char **argv, char **envp) 
+int main(int ac, char **av, char **envp) 
 {
-    int    i = 0;
+	if (ac < 2)
+		return 0;
+	int i = 0;
 
-    if (argc > 1) 
-    {
-        while (argv[i] && argv[++i]) 
-        {
-            argv += i;
-            i = 0;
-            while (argv[i] && strcmp(argv[i], "|") && strcmp(argv[i], ";"))
-                i++;
-            if (!strcmp(*argv, "cd"))
-                cd(argv, i);
-            else if (i)
-                exec(argv, envp, i);
-        }
-    }
-    return 0;
+	while (av[i] && av[++i]) 
+	{
+		av += i;
+		i = 0;
+		while (av[i] && strcmp(av[i], "|") && strcmp(av[i], ";"))
+			i++;
+		if (!strcmp(*av, "cd"))
+			cd(av, i);
+		else if (i)
+			exec(av, envp, i);
+	}
+	return 0;
 }
