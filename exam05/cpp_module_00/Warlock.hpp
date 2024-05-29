91de668cc624493b793ec1a81ad2b5f3143b78df
Jhonata Gonçalves <jhsocial@icloud.com>
2024-05-29 15:44:31 +0000

    cpp00 looking good

---
+++ exam05/cpp_module_00/Warlock.hpp
@@ -0,0 +1,24 @@
+#ifndef WARLOCK_HPP
+#define WARLOCK_HPP
+
+#include <iostream>
+#include <string>
+
+class Warlock {
+	Warlock(const Warlock &) {};
+	Warlock &operator=(const Warlock &);
+
+	std::string _name;
+	std::string _title;
+
+public:
+	Warlock(const std::string &name, const std::string &title);
+	~Warlock();
+
+	const std::string &getName() const;
+	const std::string &getTitle() const;
+	void setTitle(const std::string &title);
+	void introduce() const;
+};
+
+#endif // WARLOCK_HPP
